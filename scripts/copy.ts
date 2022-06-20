8eaadbec6d3d736c795a2d92eefad60e254aed13
Black Storm <nightyellowstormy@gmail.com>
2022-06-20 16:08:01 +0900

    make copy script

---
+++ scripts/copy.ts
@@ -0,0 +1,39 @@
+import * as fs from "fs";
+import * as path from "path";
+
+function copyFileSync(source: string, target: string) {
+
+    var targetFile = target;
+
+    if (fs.existsSync(target)) {
+        if (fs.lstatSync(target).isDirectory()) {
+            targetFile = path.join(target, path.basename(source));
+        }
+    }
+
+    fs.writeFileSync(targetFile, fs.readFileSync(source));
+}
+
+function copyFolderRecursiveSync(source: string, target: string) {
+    var files = [];
+
+    var targetFolder = path.join(target, path.basename(source));
+    if (!fs.existsSync(targetFolder)) {
+        fs.mkdirSync(targetFolder);
+    }
+
+    if (fs.lstatSync(source).isDirectory()) {
+        files = fs.readdirSync(source);
+        files.forEach(function (file) {
+            var curSource = path.join(source, file);
+            if (fs.lstatSync(curSource).isDirectory()) {
+                copyFolderRecursiveSync(curSource, targetFolder);
+            } else {
+                copyFileSync(curSource, targetFolder);
+            }
+        });
+    }
+}
+
+copyFolderRecursiveSync("artifacts", "frontend");
+copyFolderRecursiveSync("typechain", "frontend");
