17925bbfea5a568fa211d15c2ed1a3de3fdbc080
Black Storm <nightyellowstormy@gmail.com>
2022-06-20 16:14:54 +0900

    copy env variables

--- scripts/copy.ts
+++ scripts/copy.ts
@@ -1,10 +1,11 @@
 import * as fs from "fs";
 import * as path from "path";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 function copyFileSync(source: string, target: string) {
-
     var targetFile = target;
-
     if (fs.existsSync(target)) {
         if (fs.lstatSync(target).isDirectory()) {
             targetFile = path.join(target, path.basename(source));
@@ -35,5 +36,14 @@ function copyFolderRecursiveSync(source: string, target: string) {
     }
 }
 
+function copyEnvVariable() {
+    const USDC_ADDRESS = process.env.USDC_ADDRESS;
+    const TOKEN_ADDRESS = process.env.TOKEN_ADDRESS;
+    const FACTORY_ADDRESS = process.env.FACTORY_ADDRESS;
+    const env = `REACT_APP_USDC_ADDRESS=${USDC_ADDRESS}\nREACT_APP_TOKEN_ADDRESS=${TOKEN_ADDRESS}\nREACT_APP_FACTORY_ADDRESS=${FACTORY_ADDRESS}\n`;
+    fs.writeFileSync("frontend/.env", env);
+}
+
 copyFolderRecursiveSync("artifacts", "frontend");
 copyFolderRecursiveSync("typechain", "frontend");
+copyEnvVariable();
